e9cb62f51b47bbdb30cdf23d9e1e7539c5f78e45
Matti Kannisto <matti.kannisto@helsinki.fi>
2021-12-07 20:28:05 +0200

    Lisää docstringit account-luokalle

--- src/entities/account.py
+++ src/entities/account.py
@@ -1,5 +1,21 @@
 class Account:
-    def __init__(self, username, password, directory=None):
+    """A class representing a user account
+
+    Attributes:
+        username: Username of the account as a string of characters
+        password: Password of the account as a string of characters
+        directory: Directory of the account, where all output files will be saved, as a string of characters
+    """
+
+    def __init__(self, username, password, directory):
+        """Constructor of the class for creating a user account
+
+        Args:
+            username: Username of the account as a string of characters
+            password: Password of the account as a string of characters
+            directory: Directory of the account, where all output files will be saved, as a string of characters
+        """
+
         self.username = username
         self.password = password
         self.directory = directory
