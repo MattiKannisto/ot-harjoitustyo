ed55b3bd291cf31f1bc20f8493355d6c94da753a
Matti Kannisto <matti.kannisto@helsinki.fi>
2021-12-07 20:28:59 +0200

    Poista primer_pair -luokka turhana

--- src/entities/primer_pair.py
+++
@@ -1,5 +0,0 @@
-class SequencingReaction:
-    def __init__(self, template, forward_primer, reverse_primer):
-        self.template = template
-        self.forward_primer = forward_primer
-        self.reverse_primer = reverse_primer
